0a02d613eab441f2fbc6190af1323fa75ed8d896
jpiaskowski <julia.piask@gmail.com>
2026-06-10 16:06:20 -0700

    fix after simcodes debacle

--- glmmTMB_simulation_reprex.R
+++ glmmTMB_simulation_reprex.R
@@ -49,3 +49,81 @@ identical(sim0$sim_1, sim_zero$sim_1) # I thought these might be different? (the
 identical(sim0$sim_1, sim_fix$sim_1) # I thought these might be different? (they are not)
 
 
+############
+
+library(glmmTMB)
+set.seed(100)
+
+testData = DHARMa::createData(sampleSize = 1000, overdispersion = 0.5, family = poisson(), randomEffectVariance = 4)
+fittedModel <- glmmTMB(observedResponse ~ Environment1 + (1|group), 
+                       family = "poisson", data = testData)
+
+# check default for simulating data:
+for (i in seq_along(fittedModel$obj$env$data$terms)) print(fittedModel$obj$env$data$terms[[i]]$simCode)
+sim0 <- simulate(fittedModel, seed = 100, nsim = 2)
+
+# set glmmTMB sim codes
+set_simcodes(fittedModel$obj, val = "random", terms = "ALL")
+# check simulation conditions changed
+for (i in seq_along(fittedModel$obj$env$data$terms)) print(fittedModel$obj$env$data$terms[[i]]$simCode)
+# simulate data
+sim_ran <- simulate(fittedModel, seed = 100, nsim = 2) 
+
+
+set_simcodes(fittedModel$obj, val = "zero", terms = "ALL")
+for (i in seq_along(fittedModel$obj$env$data$terms)) print(fittedModel$obj$env$data$terms[[i]]$simCode)
+sim_zero <- simulate(fittedModel, seed = 100, nsim = 2)
+
+set_simcodes(fittedModel$obj, val = "fix", terms = "ALL")
+for (i in seq_along(fittedModel$obj$env$data$terms)) print(fittedModel$obj$env$data$terms[[i]]$simCode)
+sim_fix <- simulate(fittedModel, seed = 100, nsim = 2)
+
+# check glmmTMB simulations
+
+identical(sim0$sim_1, sim_ran$sim_1) # these are expected to be identical (expectation met)
+identical(sim0$sim_1, sim_zero$sim_1) # I thought these might be different? (they are not)
+identical(sim0$sim_1, sim_fix$sim_1) 
+
+### ######
+
+
+library(DHARMa)
+library(glmmTMB)
+set.seed(100)
+
+testData = createData(sampleSize = 1000, overdispersion = 5, family = poisson(), randomEffectVariance = 20)
+fittedModel <- glmmTMB(observedResponse ~ Environment1 + (1|group), 
+                       family = "poisson", data = testData)
+
+# check default for simulating data:
+for (i in seq_along(fittedModel$obj$env$data$terms)) print(fittedModel$obj$env$data$terms[[i]]$simCode)
+# dharma simulation
+simulationOutput0 <- simulateResiduals(fittedModel, plot = F, simulateREs = "conditional")
+# glmmTMB simulation
+sim0 <- simulate(fittedModel, seed = 101, nsim = 2)
+for (i in seq_along(fittedModel$obj$env$data$terms)) print(fittedModel$obj$env$data$terms[[i]]$simCode)
+
+
+simulationOutput_ran <- simulateResiduals(fittedModel, plot = F, simulateREs = "conditional")
+sim_ran <- simulate(fittedModel, seed = 101, nsim = 2) 
+for (i in seq_along(fittedModel$obj$env$data$terms)) print(fittedModel$obj$env$data$terms[[i]]$simCode)
+
+simulationOutput_fix <- simulateResiduals(fittedModel, plot = F, simulateREs = "unconditional")
+sim_fix <- simulate(fittedModel, seed = 101, nsim = 2)
+for (i in seq_along(fittedModel$obj$env$data$terms)) print(fittedModel$obj$env$data$terms[[i]]$simCode)
+
+# check DHARMa resids (these are the same) 
+res0 <- residuals(simulationOutput0)
+res_ran <- residuals(simulationOutput_ran)
+res_fix <- residuals(simulationOutput_fix)
+
+identical(res0, res_ran)  #identical (expected)
+identical(res0, res_fix) # different (expected)
+
+# check glmmTMB simulations
+
+identical(sim0$sim_1, sim_ran$sim_1) # these are expected to be identical (expectation met)
+identical(sim0$sim_1, sim_fix$sim_1) # these are different? (expectation met)
+
+
+
